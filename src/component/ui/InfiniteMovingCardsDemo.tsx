1ecb1b870d973fc0e549945acd3ddb7dcaea8bee
Salif Biaye <salifbiaye@esp.sn>
2024-07-09 18:54:13 +0000

    test:

--- src/component/ui/InfiniteMovingCardsDemo.tsx
+++ src/component/ui/InfiniteMovingCardsDemo.tsx
@@ -18,17 +18,16 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
     {
         quote:
-            "La carte ESP32 a transformé notre projet de maison intelligente en une réalité. Grâce à ses capacités de connectivité et à sa puissance de traitement, nous avons pu intégrer facilement divers capteurs et automatiser de nombreuses tâches quotidiennes.",
-        title: "carte ESP32",
+            "La carte ESP32 a transformé notre projet de maison intelligente en une réalité. Grâce à ses capacités de connectivité et à sa puissance de traitement, on l'utilise pour envoyer des notifications lorsque des capteurs détectent des mouvements ",
     },
     {
         quote:
-            "Le capteur PIR est incroyablement efficace pour détecter les mouvements dans notre système de sécurité. Il a une portée et une précision remarquables, nous alertant immédiatement de toute présence indésirable.",
+            "Le capteur PIR est incroyablement efficace pour détecter les mouvements dans notre système de sécurité. Il a une portée et une précision remarquables, nous alertant immédiatement de toute présence indésirable. en allumant des lumières ",
         title: "capteur PIR",
     },
     {
         quote:
-            "Le capteur de proximité infrarouge est un ajout fantastique à notre projet robotique. Il permet une détection précise des obstacles, ce qui est essentiel pour la navigation autonome de nos robots.",
+            "Le capteur de proximité infrarouge est un ajout fantastique à notre projet robotique. Il permet une détection précise des elements qui coupent son rayon infrarouge, ",
         title: "capteur de proximité infrarouge",
     },
 ];
