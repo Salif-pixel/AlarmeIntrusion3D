9f5b85e8242e6b47b3d46e18cbe3371f0bb03c61
Salif Biaye <salifbiaye@esp.sn>
2024-07-10 18:32:46 +0000

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             type: "spring",
             stiffness: 100,
             damping: 25,
-            delay: 6,
+            delay: 10,
         }} id={`overlay`} className={`fixed left-0 top-0 right-0 bottom-0 overflow-hidden  text-2xl text-center text-white  bg-black z-[999]`}>
             chargement...
         </motion.div>
