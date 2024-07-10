cb39a9fa4316a465c8a57874ae0daf07b8ccd660
Salif Biaye <salifbiaye@esp.sn>
2024-07-10 18:36:02 +0000

    test

--- src/component/ui/3d-cardDemo.tsx
+++ src/component/ui/3d-cardDemo.tsx
@@ -42,17 +42,7 @@ export function ThreeDCardDemo({title,description,image}: CardProps) {
                         alt="thumbnail"
                     />
                 </CardItem>
-                <div className="flex justify-between items-center mt-20">
-                    <CardItem
-                        translateZ={20}
-                        href="https://twitter.com/mannupaaji"
-                        target="__blank"
-                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                        Try now →
-                    </CardItem>
-
-                </div>
+               
             </CardBody>
         </CardContainer>
     );
