37c4c4b82f12f95ffda90768904cae86986b3012
Salif Biaye <salifbiaye@esp.sn>
2024-07-17 16:25:14 +0000

    first commit

--- src/component/ui/ThreeDIntro.tsx
+++
@@ -1,8 +0,0 @@
-
-import Spline from '@splinetool/react-spline';
-
-export default function ThreeDIntro() {
-    return (
-        <Spline className={`w-screen absolute z-10 inset-0  `} scene="https://prod.spline.design/Sss7-SAnCnrBAj34/scene.splinecode" />
-    );
-}
